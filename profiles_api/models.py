8d1053729540e24a5ef937b61a8feb91d208d9e8
BogdanMarghescu <bogdan.marghescu99@gmail.com>
2022-09-15 15:28:42 +0300

    Added profiles viewset

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -28,7 +28,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    object = UserProfileManager()
+    objects = UserProfileManager()
     USERNAME_FIELD = 'username'
     # REQUIRED_FIELDS = ['username']
 
